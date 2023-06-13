8803814384dc0d7f0c3ce7bcb03230457070089b
Mohd Shadab <shadab_ibrahim@outlook.com>
2023-06-13 18:54:28 +0530

    submitting code-Tue Jun 13 2023 18:54:28 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data,setData] = useState(0);
+  function updateData(){
+    setData(data+1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {data} times</p>
+        <button onClick={updateData}></button>
     </div>
   )
 }
