5c37d803aec9d2ca6268b2f8d0065551b1add1bb
Mohd Shadab <shadab_ibrahim@outlook.com>
2023-06-13 19:02:22 +0530

    submitting code-Tue Jun 13 2023 19:02:22 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import './../styles/App.css';
+import counter from "./counter";
 
 const App = () => {
-  const [data,setData] = useState(0);
-  function updateData(){
-    setData(data+1);
-  }
+  
   return (
     <div>
         {/* Do not remove the main div */}
-        <p>Button clicked {data} times</p>
-        <button onClick={updateData}>counter</button>
+        <counter/>
     </div>
   )
 }
